39d439c5dea4a6b302f93f12016fcecdec25d0b6
kalle (jag) <kalle.jillheden@gmail.com>
2019-04-13 19:47:25 +0200

    Improved open door script & prefab

--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -1,41 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 // Detta script ska läggas på ett objekt som spelaren ska röra för att öppna dörren. 
 
 public class OpenDoor : MonoBehaviour {
 
-    Animator animator; // Gör att dörren har en rörelse.
+    Animator animator;
 
-    bool isOpen;  // En bool som håller koll på om dörren är öppen eller inte
-    public bool isLocked = true;  // En bool som håller koll på om dörren är låst eller olåst. Från början sätter vi den till sant (för dörren är låst)
+    bool isOpen;
+    public bool isLocked = true;
 
-    public GameObject player; // Vi hämtar spelaren för att veta vilket objekt som ska kunna interagera med dörren.
+    private static readonly int ANIM_OPEN = Animator.StringToHash("Open");
+    private static readonly int ANIM_CLOSE = Animator.StringToHash("Close");
 
-    void Start() {
-        //Vis start sätter vi igång animationen så dörren kan öppnas när vi ineragerar med objektet. 
+    void OnEnable() {
         animator = transform.parent.GetComponentInChildren<Animator>();
     }
 
-
     void OnTriggerEnter(Collider other) {
-        // Om det inte är spelaren som interagerar med objektet ska inget hända.
-        if (other.gameObject != player) {
+        if (!other.CompareTag("Player")) {
             return;
         }
 
         // Det nedanstående är det som alltså händer när det är spelaren som rör objektet (triggern som ska göra att dörren öppnas).
 
-        if (isLocked == false || isLocked == true && other.GetComponent<PlayerBag>().hasKey) {
-            isOpen = !isOpen;
+        if (!isLocked || other.GetComponent<PlayerBag>().hasKey)
+        {
+            if (!isOpen)
+            {
+                isOpen = true;
 
-            if (isOpen == true) {
-                animator.SetTrigger("Open");
+                animator.SetTrigger(ANIM_OPEN);
             }
-            else {
-                animator.SetTrigger("Close");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            return;
+        }
+
+        if (!isLocked || other.GetComponent<PlayerBag>().hasKey)
+        {
+            if (isOpen)
+            {
+                isOpen = false;
+                animator.SetTrigger(ANIM_CLOSE);
             }
         }
     }
